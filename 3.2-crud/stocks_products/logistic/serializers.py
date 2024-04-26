bf5ee0a3684d646d162dfdb3e95c42dfb7af8c74
timur555 <timur.tsedik@gmail.com>
2024-04-26 14:59:09 +0300

    hw

--- 3.2-crud/stocks_products/logistic/serializers.py
+++ 3.2-crud/stocks_products/logistic/serializers.py
@@ -1,32 +1,42 @@
 from rest_framework import serializers
 
+from logistic.models import Product, StockProduct, Stock
+
 
 class ProductSerializer(serializers.ModelSerializer):
     # настройте сериализатор для продукта
+    class Meta:
+        model = Product
+        fields = '__all__'
     pass
 
 
 class ProductPositionSerializer(serializers.ModelSerializer):
     # настройте сериализатор для позиции продукта на складе
+    class Meta:
+        model = StockProduct
+        fields = '__all__'
     pass
 
 
 class StockSerializer(serializers.ModelSerializer):
+
     positions = ProductPositionSerializer(many=True)
 
     # настройте сериализатор для склада
 
     def create(self, validated_data):
         # достаем связанные данные для других таблиц
-        positions = validated_data.pop('positions')
+        positions = validated_data.pop('positions', [])
 
         # создаем склад по его параметрам
         stock = super().create(validated_data)
-
         # здесь вам надо заполнить связанные таблицы
         # в нашем случае: таблицу StockProduct
         # с помощью списка positions
-
+        for position in positions:
+            position['stock'] = stock
+            StockProduct.objects.create(**position)
         return stock
 
     def update(self, instance, validated_data):
@@ -39,5 +49,15 @@ class StockSerializer(serializers.ModelSerializer):
         # здесь вам надо обновить связанные таблицы
         # в нашем случае: таблицу StockProduct
         # с помощью списка positions
-
+        for position in positions:
+            created = StockProduct.objects.update_or_create(
+                stock=stock,
+                product=position.get('product'),
+                quantity=position.get('quantity'),
+                price=position.get('price')
+            )
         return stock
+
+    class Meta:
+        model = Stock
+        fields = '__all__'
--- 3.2-crud/stocks_products/logistic/views.py
+++ 3.2-crud/stocks_products/logistic/views.py
@@ -1,16 +1,28 @@
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter
+from rest_framework.filters import OrderingFilter
 from rest_framework.viewsets import ModelViewSet
 
-from logistic.models import Product, Stock
-from logistic.serializers import ProductSerializer, StockSerializer
+
+from .models import Product, Stock
+from .serializers import ProductSerializer, StockSerializer
 
 
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     # при необходимости добавьте параметры фильтрации
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filterset_fields = ['title', 'description']
+    search_fields = ['title', 'description']
+    ordering_fields = ['id', 'title']
 
 
 class StockViewSet(ModelViewSet):
     queryset = Stock.objects.all()
     serializer_class = StockSerializer
     # при необходимости добавьте параметры фильтрации
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filterset_fields = ['address', 'products']
+    search_fields = ['address', 'products']
+    ordering_fields = ['id', 'address', 'quantity']
